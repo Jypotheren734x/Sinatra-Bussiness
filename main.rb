7d0ae3cfd9c15ef852123ffb473491ade9e7786f
Nick <komarnickolas@gmail.com>
2017-06-23 13:35:51 -0600

    Updates

--- main.rb
+++ main.rb
@@ -1,4 +1,5 @@
 require 'sinatra'
+require 'pony'
 
 set :public_folder, 'public'
 
@@ -12,4 +13,20 @@ end
 
 get '/manufacturers' do
   erb :manufacturers
+end
+
+get '/contact' do
+  erb :contact
+end
+
+post '/contact' do
+  @name = params[:name]
+  @email = params[:email]
+  @message = params[:message]
+  Pony.mail(
+      to: 'komarnickolas@gmail.com',
+      from: @email.to_s,
+      subject: @name.to_s + "has contacted you",
+      body: @message.to_s
+  )
 end
